30439436a389256986487f7f988d443c0f5d4925
eagleweb <eagleweb@mail.ru>
2017-11-13 19:31:11 +0200

    Add

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<img class="slider_button_next" src="./img/slider-button-next.png" alt="">',
+        prevArrow: '<img class="slider_button_prev" src="./img/slider-button-prev.png" alt="">',
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+});﻿
