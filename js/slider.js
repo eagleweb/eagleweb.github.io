9deeb28472764c1f1278e995d4570d982f743f4b
eagleweb <eagleweb@mail.ru>
2017-11-15 12:16:22 +0200

    Add

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.slider').slick({
         nextArrow: '<img class="slider_button_next" src="./img/slider-button-next.png" alt="">',
-        prevArrow: '<img class="slider_button_prev" src="./img/slider-button-prev.png" alt="">',
+        prevArrow: false,
         autoplay: true,
         autoplaySpeed: 5000
     });
