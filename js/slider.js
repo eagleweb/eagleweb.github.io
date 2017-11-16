5e900eef77aee8c0c953be39688a8a2ac2e18809
eagleweb <eagleweb@mail.ru>
2017-11-16 17:16:54 +0200

    Устранение замечаний после урока 15.11

--- js/slider.js
+++ js/slider.js
@@ -2,6 +2,8 @@ $(function(){
     $('.slider').slick({
         nextArrow: '<img class="slider_button_next" src="./img/slider-button-next.png" alt="">',
         prevArrow: false,
+        dots: true,
+        dotsClass: 'dots_slider',
         autoplay: true,
         autoplaySpeed: 5000
     });
